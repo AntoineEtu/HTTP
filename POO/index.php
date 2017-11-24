710ef80badd6c0bdcec3a19afb4eabde695c1d8e
LAPTOP-VOD70US0\antoi <antoinekeribin@hotmail.fr>
2017-11-24 16:53:13 +0100

    Petit bonus pour mieux comprendre :)

--- POO/index.php
+++ POO/index.php
@@ -9,7 +9,21 @@
 		<div><?php
 		require('triangle.php');
 		$triangle1 = new Triangle(1,1,sqrt(2));
+		echo('Premier Triangle : 1,1,sqrt(2)  ');
 		$triangle1->findTrianlgeType();
+		echo('<br>');
+		$triangle2 = new Triangle(3,4,5);
+		echo('Deuxieme Triangle : 3,4,5  ');
+		$triangle2->findTrianlgeType();
+		echo('<br>');
+		$triangle3 = new Triangle(1,1,4);
+		echo('Troisieme Triangle : 1,1,4  ');
+		$triangle3->findTrianlgeType();
+		echo('<br>');
+		$triangle4 = new Triangle(1,1,1);
+		echo('Quatrième Triangle : 1,1,1  ');
+		$triangle4->findTrianlgeType();
+		echo('<br>');
 			?></div>
 	</body>
 
